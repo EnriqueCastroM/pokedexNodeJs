725448e227435bf8d8f778e0f0a3c0a1d8df730f
IvanCastro <kcmtha@gmail.com>
2022-07-28 13:37:52 -0500

    paginacion

--- scr/public/js/script.js
+++ scr/public/js/script.js
@@ -6,6 +6,62 @@ const pokeId = document.querySelector('[data-poke-id]');
 const pokeTypes = document.querySelector('[data-poke-types]');
 const pokeStats = document.querySelector('[data-poke-stats]');
 
+const lists__pokemons = document.getElementById('lists__pokemons')
+const buttons = document.getElementById('buttons')
+let urlPokemon = ' https://pokeapi.co/api/v2/pokemon'
+let btnNext;
+let btnPrevious;
+let templateHtml;
+console.log('⏮⏩')
+
+const GetPokemons = async (url) => {
+    try {
+        const response = await fetch(url)
+        const results = await response.json();
+        console.log(results)
+        DataPokemons(results.results)
+
+        btnNext=results.next ? `<button class="btn" data-url=${results.next}>⏩</button>` : ''
+        btnPrevious=results.previous ? `<button class="btn" data-url=${results.previous}>⏮</button>` : ''
+        buttons.innerHTML=btnPrevious + " " + btnNext
+        
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+GetPokemons(urlPokemon)
+
+const DataPokemons = async (data) => {
+    lists__pokemons.innerHTML = '';
+    try {
+        for (let index of data) {
+
+            const resp = await fetch(index.url)
+            const resul = await resp.json();
+            console.log(resul)
+            templateHtml=`
+            <div class="pokemon__img">
+            <img src=${resul.sprites.other.dream_world.front_default} alt=${resul.name}/>
+            <p>${resul.name}</p>
+            </div>
+            `
+            lists__pokemons.innerHTML+=templateHtml
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+buttons.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('btn')){
+        let value=e.target.dataset.url
+        console.log(value)
+        GetPokemons(value)
+    }
+})
+
 const typeColors = {
     electric: '#FFEA70',
     normal: '#B09398',
@@ -36,6 +92,15 @@ const searchPokemon = event => {
         .catch(err => renderNotFound())
 }
 
+const imagePokemon = event => {
+    event.preventDefault();
+    const { value } = event.target.pokemon;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+        .then(data => data.json())
+        .then(response => renderPokemonImg(response))
+        .catch(err => renderNotFound())
+}
+
 const renderPokemonData = data => {
     const sprite =  data.sprites.front_default;
     const { stats, types } = data;
@@ -46,6 +111,15 @@ const renderPokemonData = data => {
     setCardColor(types);
     renderPokemonTypes(types);
     renderPokemonStats(stats);
+}
+
+const renderPokemonImg = data => {
+    const sprite =  data.sprites.front_default;
+    const { stats, types } = data;
+
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    
 }
 
 
@@ -82,7 +156,7 @@ const renderPokemonStats = stats => {
 
 const renderNotFound = () => {
     pokeName.textContent = 'No encontrado';
-    pokeImg.setAttribute('src', 'poke-shadow.png');
+    pokeImg.setAttribute('src', '');
     pokeImg.style.background =  '#fff';
     pokeTypes.innerHTML = '';
     pokeStats.innerHTML = '';
